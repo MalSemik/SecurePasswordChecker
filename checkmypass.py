3c74b0082c311426d4f1c1810b0f0a6433d27693
Malgorzata <malg.sem@gmail.com>
2020-10-02 18:48:10 +0200

    create pwned_api_check function

--- checkmypass.py
+++ checkmypass.py
@@ -9,16 +9,26 @@ def request_api_data(query_char):
         raise RuntimeError(f'Error fetching: {res.status_code}')
     return res
 
-def get_password_leaks_conut(hashes, hash_to_check):
+
+def get_password_leaks_count(hashes, hash_to_check):
     hashes = (line.split(':') for line in hashes.text.splitlines())
     for hash, count in hashes:
-        print(hash, count)
+        if hash == hash_to_check:
+            # print(count)
+            return count
+    return 0
+
+
 def pwned_api_check(password):
     sha1password = hashlib.sha1(password.encode('utf-8')).hexdigest().upper()
+    # print(sha1password)
     first_5_char, rest_of_hash = sha1password[:5], sha1password[5:]
     response = request_api_data(first_5_char)
-    print(first_5_char, rest_of_hash)
-    return get_password_leaks_conut(response, rest_of_hash)
+    leaks_count = get_password_leaks_count(response, rest_of_hash)
+    if leaks_count == 0:
+        print("This password never leaked, you're good to go!")
+    else:
+        print(f'This password leaked {leaks_count} times.')
 
 # request_api_data('CDAFE')
-pwned_api_check('123')
+pwned_api_check('admin123')
